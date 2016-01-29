b39b8456ad3ffaf647205307f38a9f7cc240d35f
Kevin Bowe <kevinbowe1957@gmail.com>
2016-01-29 16:57:03 -0500

    Copy All Existing Code
    NOTE:
    — Set stock price to Random
    — Removed ‘StockDog Path’ from Desktop and Watchlist links in index.html
    
    Verified that this code works as
        http://localhost:9000/#/dashboard

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,27 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name sdApp
- * @description
- * # sdApp
- *
- * Main module of the application.
- */
 angular
-  .module('sdApp', [
+  .module('stockDogApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'mgcrea.ngStrap',
+    'googlechart'
   ])
   .config(function ($routeProvider) {
     $routeProvider
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
+      })
+      .when('/watchlist/:listId', {
+        templateUrl: 'views/watchlist.html',
+        controller: 'WatchlistCtrl'
+      })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/dashboard'
       });
   });
